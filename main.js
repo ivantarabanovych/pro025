2732a4ca183bcfab14a3479871840ac68269dac4
ivantarabanovych <vanjokcitet334@gmail.com>
2024-09-25 23:43:35 +0300

    q2

--- main.js
+++ main.js
@@ -14,18 +14,27 @@
 // printNumbers(1, 10);
 
 
-function printNumbers(from, to){
-    let current = from;
+// function printNumbers(from, to){
+//     let current = from;
 
-    function go(){
-        console.log(current);
+//     function go(){
+//         console.log(current);
 
-        if(current < to){
-            setTimeout(go, 1000);
-        }
-        current++;
-    }
-    setTimeout(go, 1000)
-}
+//         if(current < to){
+//             setTimeout(go, 1000);
+//         }
+//         current++;
+//     }
+//     setTimeout(go, 1000)
+// }
 
-printNumbers(1, 10);
+// printNumbers(1, 10);
+
+let i = 0;
+
+setTimeout(() => alert(i), 100); // ?
+
+for (let j = 0; j < 100000000; j++) {
+  i++;
+} 
+// функція запуститься після циклу, буде виведено 100000000
